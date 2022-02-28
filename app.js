17f361664c03b3cf340bb0ef0305e0c857786b7f
bibisung <schmidtiana95@gmail.com>
2022-02-28 21:02:40 +0900

    20200228

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const startTime = () => {
   const time = new Date();
+  const day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+  const title = `Pillsang's power clock!!`
 
   let meridiem = '';
   let hours = 0;
@@ -25,33 +27,25 @@ const startTime = () => {
     minutes = time.getMinutes();
   }
 
-  if (time.getMinutes() / 10 < 1) {
-    minutes = `0${time.getMinutes()}`;
-  } else {
-    minutes = time.getMinutes();
-  }
-
   if (time.getSeconds() / 10 < 1) {
     seconds = `0${time.getSeconds()}`;
   } else {
     seconds = time.getSeconds();
   }
 
-  const yearElement = document.querySelector('.clock--year p');
-  const monthElement = document.querySelector('.clock--month p');
-  const dayElement = document.querySelector('.clock--day p');
+  const titleElement = document.querySelector('.title p');
   const meridiemElement = document.querySelector('.clock--meridiem p');
   const hoursElment = document.querySelector('.clock--hours p');
   const minutesElment = document.querySelector('.clock--minutes p');
   const secondsElment = document.querySelector('.clock--seconds p');
+  const dateElement = document.querySelector('.date p');
 
-  yearElement.innerHTML = time.getFullYear();
-  monthElement.innerHTML = months[time.getMonth()];
-  dayElement.innerHTML = time.getDate();
+  titleElement.innerHTML = title;
   meridiemElement.innerHTML = meridiem;
   hoursElment.innerHTML = hours;
   minutesElment.innerHTML = minutes;
   secondsElment.innerHTML = seconds;
+  dateElement.innerHTML = `${day[time.getDay()]}, ${months[time.getMonth()]} ${time.getDate()} ${time.getFullYear()}`;
 
   setTimeout(startTime, 1000);
 };
